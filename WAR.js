fed9a9a20ba36fad32e536aa773a9d22ee871825
GingerThrust <mbrown9388@gmail.com>
2022-09-09 14:24:32 -0500

    NEW FINAL assignment work

--- WAR.js
+++ WAR.js
@@ -25,12 +25,14 @@ function shuffleAndDeal() {
     player1.push(masterDeck.slice(0,25));
     player2.push(masterDeck.slice(26,51));
 }
-
+//At this point, I have to admit defeat. Every code I have below is my attempt at making this game. I've gotten it to a point where a console.log is not showing any syntax errors. But I can't seem to get it to go
+//I understand the assignment is incomplete. I'd rather turn something in than nothing at all. But my understanding of Objects and Classes and when to use those vs Functions is just not good. 
+//I will try to comment my logic behind things in the white spaces between the code blocks. 
 class WAR {
     constructor () {
         this.round = [];
     }
-
+//Here I just wasn't sure what to build in the constructor part. I didn't really feel like I needed anything, but had to put something. 
 
     NewTurn() {
         this.round.push(player1.shift());
@@ -46,7 +48,9 @@ class WAR {
         this.round = [];
     }
       
-
+//In the code above, my reasoning was that I made an array "this.round". After calling the shuffleAndDeal function above, the arrays of player1 and player2 should have all the cards. So then I wanted to 
+//take off the first index of those player arrays and put them into the "this.round" array. I assumed that if I did that for player1 first and then player2, we can assume the first index or card in the
+//array would have come from player1. So then I tried to compare the two items in the index and award a point based off the result of the if/then statement. 
     startGame() {
          let turn = this.NewTurn(); 
          do {
@@ -54,7 +58,7 @@ class WAR {
         } while ((player1.length !== 0) && (player2.length !== 0));
          
           
-        
+      //Above, I was trying to define turn as the method I build above. And have the method work while the arrays of each player were not empty.   
         if (player1Point > player2Point) {
         return `Player 1 wins with ${player1Point} points!`;
        } else {
@@ -62,8 +66,11 @@ class WAR {
        }
     }
 }
-
+//Above, I figured that if at the end of the do/while statement, we do a comparison on the points and award a winner. 
 let game = new WAR();
 shuffleAndDeal(masterDeck);
 game.NewTurn();
 console.log(startGame());
+//Here is where I can't seem to make anything come up
+
+//Finally, as I can't seem to make the game work, I didn't build a Unit Test for it. So I'll take responsibility for points lost on that. 
